cfb113a234d9a0fa266609c68bae104f49130a79
Bhavesh Kumar Lohar <bhaveshsisodia2@gmail.com>
2026-05-07 12:29:14 +0530

    folder structure created

---
+++ template.sh
@@ -0,0 +1,18 @@
+
+## directory structure
+mkdir -p src
+mkdir -p research
+
+## files
+touch src/_init__.py
+touch src/main.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/_init__.py
+touch research/trails.ipynb
+touch requirements.txt
+
+echo "Project structure created successfully!"
